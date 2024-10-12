0079a56b3ee6458aa7e2fcae75d4ecf3528f1cc0
mainstreamed <106301048+mainstreamed@users.noreply.github.com>
2024-10-12 20:01:11 +1000

    Update gradient_drawing.lua

--- libs/gradient_drawing.lua
+++ libs/gradient_drawing.lua
@@ -1,35 +1,59 @@
-local table_insert = table.insert;
-local table_remove = table.remove;
+--[[
+      TODO:
+      > support negative sizes
+      > support more than 2 colours
+      > support horizontal gradients (doubt)
+]]
 
-local math_ceil   = math.ceil;
+local table_insert      = table.insert;
+local table_remove      = table.remove;
+
+local math_ceil         = math.ceil;
+local math_round        = math.round;
+local math_max          = math.max;
+local math_min          = math.min;
+
+local string_format     = string.format;
+
+local vector2_new       = Vector2.new;
+local colour3_new       = Color3.new;
+local drawing_new       = Drawing.new;
+
+local lerp              = colour3_new().Lerp;
+local error             = error;
 
 local cached_squares = _G.cached_squares or {};
 _G.cached_squares = cached_squares;
 
-local square_mt = getrawmetatable(Drawing.new('Square'));
-local __index = square_mt.__index;
+local square_mt = getrawmetatable and getrawmetatable(Drawing.new('Square')) or getmetatable(Drawing.new('Square'));
 local __newindex = square_mt.__newindex;
 
 local create_gradient = function()
+      local drawingobject     = newproxy(true);
+      local metatable         = getmetatable(drawingobject);
+
       local properties = {
             Visible = false;
             Transparency = 1;
             ZIndex = 0;
-
-            ColorStart = Color3.new();
-            ColorEnd = Color3.new();
-            PixelSize = 2;
-            Size = Vector2.new();
-            Position = Vector2.new();
+            ColorStart = colour3_new();
+            ColorEnd = colour3_new();
+            Size = vector2_new();
+            Position = vector2_new();
       };
-      local hidden_properties = {
-            required_squares = (math_ceil(properties.Size.Y) + properties.PixelSize / 2) // properties.PixelSize;
-            cached_squares = {};
+      local hidden = {
+            size = 1;
+            amount = 0;
+            squares = {};
       };
 
+      local should_show = function()
+            local size = properties.Size;
+            return properties.Visible and size.X > 0 and size.Y > 0 and properties.Transparency > 0;
+      end;
       local update_position = function()
             local position = properties.Position;
-            local squares = hidden_properties.cached_squares;
+            local squares = hidden.squares;
 
             if (not squares[1]) then
                   return;
@@ -41,32 +65,38 @@ local create_gradient = function()
             end;
       end;
       local update_size = function()
-            local position = properties.Position;
-            local size = properties.Size;
-            local pixels_left = size.Y;
-            local pixel_size = properties.PixelSize;
-            local squares = hidden_properties.cached_squares;
+            local position    = properties.Position;
+            local size        = properties.Size;
+            local colourstart = properties.ColorStart;
+            local colourend   = properties.ColorEnd;
 
-            for i = 1, #squares do
+            local pixelsize   = hidden.size;
+            local pixelremain = size.Y;
+
+            local squares     = hidden.squares;
+            local amount      = hidden.amount;
+
+            for i = 1, amount do
                   local square = squares[i];
                   
-                  __newindex(square, 'Position', position + Vector2.new(0, (i-1)*pixel_size));
-                  __newindex(square, 'Size', Vector2.new(size.X, math.min(pixel_size, pixels_left)));
-                  __newindex(square, 'Color', properties.ColorStart:Lerp(properties.ColorEnd, i / #squares));
+                  __newindex(square, 'Position', position + vector2_new(0, (i-1) * pixelsize));
+                  __newindex(square, 'Size', vector2_new(size.X, math_min(pixelsize, pixelremain)));
+                  __newindex(square, 'Color', lerp(colourstart, colourend, i / amount));
 
-                  pixels_left -= pixel_size;
+                  pixelremain -= pixelsize;
             end;
       end;
-      local recache_squares = function()
-            local squares = hidden_properties.cached_squares;
-            local current = #squares;
-            local amount = hidden_properties.required_squares - current;
+      local refresh_squares = function()
+            local squares     = hidden.squares;
+            local current_amt = #squares;
 
-            if (amount == 0) then
+            local required    = hidden.amount - current_amt;
+            if (required == 0) then
                   return;
-            elseif (amount < 0) then
-                  amount = -amount;
-                  for i = current, current-amount+1, -1 do
+            elseif (required < 0) then
+                  -- removing squares
+                  required = -required;
+                  for i = current_amt, current_amt - required + 1, -1 do
                         local square = squares[i];
                         __newindex(square, 'Visible', false);
                         table_insert(cached_squares, square);
@@ -74,101 +104,131 @@ local create_gradient = function()
                   end;
                   return;
             end;
-            for i = 1, amount do
+            -- adding more squares
+            for i = 1, required do
                   local square = cached_squares[i];
                   if (square) then
                         table_remove(cached_squares, i);
                   else
-                        square = Drawing.new('Square');
+                        square = drawing_new('Square');
                         __newindex(square, 'Filled', true);
                         __newindex(square, 'Thickness', 1);
                   end;
                   __newindex(square, 'Visible', true);
                   __newindex(square, 'Transparency', properties.Transparency);
                   __newindex(square, 'ZIndex', properties.ZIndex);
-
                   table_insert(squares, square);
             end;
       end;
-      local free_squares = function()
-            local squares = hidden_properties.cached_squares;
-            for i = 1, #squares do
+      local remove_squares = function()
+            local squares = hidden.squares;
+            for i = 1, hidden.amount do
                   local square = squares[i];
                   __newindex(square, 'Visible', false);
                   table_insert(cached_squares, square);
             end;
-            hidden_properties.cached_squares = {};
+            hidden.squares = {};
       end;
 
-      local drawingObject = newproxy(true);
-      local metatable = getmetatable(drawingObject);
-
-      metatable.__metatable = 'The metatable is locked';
-      metatable.__index = function(self, index)
+      local __index = function(self, index)
             if (index == 'Remove' or index == 'Destroy') then
-                  return function(self)
-                        free_squares();
-                        metatable.__newindex = function()
+                  return function()
+                        remove_squares();
+
+                        local onindex           = function()
                               error('DrawingObject no longer exists');
                         end;
-                        metatable.__index = function() 
-                              error('DrawingObject no longer exists');
-                        end;
+                        metatable.__newindex    = onindex;
+                        metatable.__index       = onindex;
                   end;
             end;
             return properties[index];
       end;
-      metatable.__newindex = function(self, index, newindex)
-            if (properties[index] == nil) then
-                  return error(string.format('%* is not a valid member of %*', index, self));
-            elseif (typeof(properties[index]) ~= typeof(newindex)) then
-                  return error(string.format('invalid property type on %* (%* expected, got %*)', index, typeof(properties[index]), typeof(newindex)));
-            elseif (newindex == properties[index]) then
+      local __newindex = function(self, index, newindex)
+            local old_value = properties[index];
+            if (old_value == nil) then
+                  return error( string_format('%* is not a valid member of DrawingObject', index) );
+            elseif (typeof(old_value) ~= typeof(newindex)) then
+                  return error( string_format('invalid property type for %* (%* expected, got %*)', index, typeof(old_value), typeof(newindex)) );
+            elseif (newindex == old_value) then
                   return;
             end;
 
+            local squares = hidden.squares;
+            local is_showing = #squares > 0;
+
             properties[index] = newindex;
 
-            if (index == 'Size' or index == 'PixelSize') then
+            if (index == 'Size') then
                   local size = properties.Size;
-
-                  local pixelsize = properties.PixelSize;
-
-                  hidden_properties.required_squares = (math_ceil(size.Y) + pixelsize / 2) // pixelsize;
-                  if (properties.Visible and size.Y > 0 and size.X > 0) then
-                        recache_squares();
+                  local pixelsize = math_round(math_max(size.Y / 11.5, 3));
+                  
+                  hidden.size = pixelsize;
+                  hidden.amount = (math_ceil(size.Y) + pixelsize / 2) // pixelsize;
+                  
+                  if (size.Y <= 0 or size.X <= 0) then
+                        remove_squares();
+                  elseif (properties.Visible and properties.Transparency > 0) then
+                        refresh_squares();
                         update_size();
-                  elseif (size.Y <= 0 or size.X <= 0) then
-                        free_squares();
-                  end;
-            elseif (index == 'Position') then
-                  if (properties.Visible) then
-                        update_position();
                   end;
             elseif (index == 'Visible') then
-                  if (newindex) then
-                        recache_squares();
+                  if ( should_show() ) then
+                        refresh_squares();
                         update_size();
                   else
-                        free_squares();
+                        remove_squares();
                   end;
-            elseif ((index == 'Transparency' or index == 'ZIndex') and properties.Visible) then
+            elseif (index == 'Position' and is_showing) then
+                  update_position();
+            elseif ((index == 'Transparency' or index == 'ZIndex') and is_showing) then
                   if (properties.Transparency <= 0) then
-                        free_squares();
+                        remove_squares();
                   else
-                        local squares = hidden_properties.cached_squares;
                         for i = 1, #squares do
                               __newindex(squares[i], index, newindex);
                         end;
                   end;
-            elseif ( (index == 'ColorStart' or index == 'ColorEnd') and properties.Visible) then
-                  local squares = hidden_properties.cached_squares;
-                  for i = 1, #squares do
-                        __newindex(squares[i], 'Color', properties.ColorStart:Lerp(properties.ColorEnd, i/#squares));
+            elseif ( (index == 'ColorStart' or index == 'ColorEnd') and is_showing) then
+                  local amount = hidden.amount;
+                  for i = 1, amount do
+                        __newindex(squares[i], 'Color', lerp(properties.ColorStart, properties.ColorEnd, i / amount));
                   end;
             end;
       end;
-      return drawingObject;
+
+      metatable.__metatable   = 'The metatable is locked';
+      metatable.__index       = __index;
+      metatable.__newindex    = __newindex;
+
+      return drawingobject;
 end;
 
-return create_gradient;
+-- Drawing.new implemenation
+local integrate_gradient = function() -- call me to integrate Gradient's into real drawing
+      local setreadonly = setreadonly;
+      
+      local old_drawingnew = Drawing.new;
+      local isreadonly = table.isfrozen(Drawing);
+      if (not isreadonly) then
+            setreadonly = function() end;
+      end;
+      
+      setreadonly(Drawing, false);
+
+      Drawing.new = function(t)
+            if (t == 'Gradient') then
+                  return create_gradient();
+            end;
+            return old_drawingnew(t);
+      end;
+      
+      setreadonly(Drawing, isreadonly);
+end;
+
+
+if (...) then
+      integrate_gradient();
+end;
+
+return create_gradient, integrate_gradient;
